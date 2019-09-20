101ebe290671338a30adb268234c5e3b8c829053
helmybasri <47407846+helmybasri@users.noreply.github.com>
2019-09-21 00:42:13 +0700

    -

--- database/migrations/2019_03_25_140349_create_brands_table.php
+++ database/migrations/2019_03_25_140349_create_brands_table.php
@@ -18,6 +18,7 @@ class CreateBrandsTable extends Migration
           $table->string('name');
           $table->string('email');
           $table->string('website');
+          $table->string('socmed');
           $table->string('socmed_link');
           $table->string('phone_number')->nullable();
           $table->timestamp('email_verified_at')->nullable();
