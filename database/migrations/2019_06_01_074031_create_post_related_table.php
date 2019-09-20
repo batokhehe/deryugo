00694a9210594f5f490271d5841626aad814bfd1
Naufal <naufal.mahardika@hotmail.com>
2019-09-21 00:04:01 +0700

    -

--- database/migrations/2019_06_01_074031_create_post_related_table.php
+++ database/migrations/2019_06_01_074031_create_post_related_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class PostRelatedTable extends Migration
+class CreatePostRelatedTable extends Migration
 {
     /**
      * Run the migrations.
